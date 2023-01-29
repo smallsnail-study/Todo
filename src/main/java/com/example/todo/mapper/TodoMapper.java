0d43e63a751b0a832ba2463d03c60ad8da6f124a
yunhuiGwak <kwakyh17@naver.com>
2023-01-29 17:13:23 +0900

    추가: Todo 목록 출력 기능 가장 최근데 등록된 TodoVO가 우선적으로 나올 수 있도록 selectAll()을 추가

--- src/main/java/com/example/todo/mapper/TodoMapper.java
+++ src/main/java/com/example/todo/mapper/TodoMapper.java
@@ -2,10 +2,15 @@ package com.example.todo.mapper;
 
 import com.example.todo.domain.TodoVO;
 
+import java.util.List;
+
 public interface TodoMapper { //MyBatis와 스프링을 연동(2. Mapper인터페이스의 개발)
 
     String getTime();
 
     // Todo등록기능(TodoVO를 파라미터로 입력받는 insert()추가)
     void insert(TodoVO todoVO);
+
+    // Todo 목록기능 개발(가장 최근에 등록된 TodoVO가 우선적으로 나올수 있도록 selectAll()을 추가한다)
+    List<TodoVO> selectAll();
 }
