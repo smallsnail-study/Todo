47fd43a915e55ddc09919981997b18e4b9e9e5d5
yunhuiGwak <kwakyh17@naver.com>
2023-01-29 15:04:13 +0900

    추가: Todo 등록 (3.Controller) model을 받도록 추가, POST방식으로 처리 후 redirect경로추가

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -2,11 +2,12 @@ package com.example.todo.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.todo.dto.TodoDTO;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/todo")
@@ -14,7 +15,8 @@ import com.example.todo.dto.TodoDTO;
 public class TodoController {
 
     @RequestMapping("/list")    // 최종경로 /todo/list
-    public void list() {
+    public void list(Model model) {
+
         log.info("todo list.............");
     }
 
@@ -26,12 +28,15 @@ public class TodoController {
     //  Todo 등록을 GET방식과 POST방식으로 나눠서 설정
     @GetMapping("register") // GET방식으로 /todo/register를 이용하면 입력 가능한 화면을 보여준다.
     public void registerGET() {
+
         log.info("GET todo register.........");
     }
 
     @PostMapping("/register")   // POST방식으로 Todo등록을 처리한다.
-    public void registerPOST(TodoDTO todoDTO) {     // 파라미터를 TodoDTO로 사용한다.(자동형변환)
+    public String registerPOST(TodoDTO todoDTO, RedirectAttributes redirectAttributes) {     // 파라미터를 TodoDTO로 사용한다.(자동형변환)
         log.info("POST todo register..............");
         log.info(todoDTO);
+
+        return "redirect:/todo/list";   // POST방식으로 처리 후 /todo/list로 이동할 수 있도록한다.
     }
 }
