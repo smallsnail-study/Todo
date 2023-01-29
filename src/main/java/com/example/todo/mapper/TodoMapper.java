87bfabfcea746c41815635a2810bbfb96ea8b059
yunhuiGwak <kwakyh17@naver.com>
2023-01-29 13:42:12 +0900

    기능: Todo 등록 기능 추가(TodoVO 파라미터)

--- src/main/java/com/example/todo/mapper/TodoMapper.java
+++ src/main/java/com/example/todo/mapper/TodoMapper.java
@@ -1,6 +1,11 @@
 package com.example.todo.mapper;
 
+import com.example.todo.domain.TodoVO;
+
 public interface TodoMapper { //MyBatis와 스프링을 연동(2. Mapper인터페이스의 개발)
 
     String getTime();
+
+    // Todo등록기능(TodoVO를 파라미터로 입력받는 insert()추가)
+    void insert(TodoVO todoVO);
 }
