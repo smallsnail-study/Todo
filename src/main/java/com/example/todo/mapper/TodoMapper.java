d56cf10fc3fba3282b2910226144d6df66598355
yunhuiGwak <kwakyh17@naver.com>
2023-01-31 13:58:23 +0900

    추가: Todo 조회 기능 추가(1.TodoMapper) selectOne() 파라미터는 Long타입의 tno를 받아서 TodoVO객체를 반환한다.

--- src/main/java/com/example/todo/mapper/TodoMapper.java
+++ src/main/java/com/example/todo/mapper/TodoMapper.java
@@ -13,4 +13,7 @@ public interface TodoMapper { //MyBatis와 스프링을 연동(2. Mapper인터�
 
     // Todo 목록기능 개발(가장 최근에 등록된 TodoVO가 우선적으로 나올수 있도록 selectAll()을 추가한다)
     List<TodoVO> selectAll();
+
+    // Todo 조회 기능 개발(목록화면에서 제목을 눌렀을 때 조회 기능)
+    TodoVO selectOne(Long tno); // 파라미터는 Long타입의 tno로 받고 TodoVO객체를 반환
 }
