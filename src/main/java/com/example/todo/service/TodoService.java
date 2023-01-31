7a71068b8f41548db1fa0dcff741350370b30840
yunhuiGwak <kwakyh17@naver.com>
2023-01-31 13:33:57 +0900

    추가: Todo 목록 출력 기능 추가(2.Service 계층설계) List<TodoDTO> getAll();

--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -2,7 +2,12 @@ package com.example.todo.service;
 
 import com.example.todo.dto.TodoDTO;
 
+import java.util.List;
+
 public interface TodoService {  // Todo 등록작업 (2.Mapper와 Controller 사이의 Service계층 설계)
 
     void register(TodoDTO todoDTO); // 여러개의 파라미터 대신에 TodoDTO로 묶어서 전달받도록 한다.
+
+    // Todo 목록 기능
+    List<TodoDTO> getAll();
 }
