ae5147d5f5ebad4bf17e593b5d27bf214cb84e94
yunhuiGwak <kwakyh17@naver.com>
2023-02-03 16:09:45 +0900

    추가: Todo 페이징 처리된 목록 기능 추가(Service) PageResponseDTO를 반환타입으로 하는 getList() 구성

--- src/main/java/com/example/todo/service/TodoService.java
+++ src/main/java/com/example/todo/service/TodoService.java
@@ -1,5 +1,7 @@
 package com.example.todo.service;
 
+import com.example.todo.dto.PageRequestDTO;
+import com.example.todo.dto.PageResponseDTO;
 import com.example.todo.dto.TodoDTO;
 
 import java.util.List;
@@ -9,7 +11,10 @@ public interface TodoService {  // Todo 등록작업 (2.Mapper와 Controller 사
     void register(TodoDTO todoDTO); // 여러개의 파라미터 대신에 TodoDTO로 묶어서 전달받도록 한다.
 
     // Todo 목록 기능
-    List<TodoDTO> getAll();
+    // List<TodoDTO> getAll();
+
+    // 페이징을 적용한 Todo 목록 기능
+    PageResponseDTO<TodoDTO> getList(PageRequestDTO pageRequestDTO);
 
     // Todo 조회 기능
     TodoDTO getOne(Long tno);
@@ -19,4 +24,6 @@ public interface TodoService {  // Todo 등록작업 (2.Mapper와 Controller 사
 
     // Todo 수정 기능
     void modify(TodoDTO todoDTO);
+
+
 }
