0ede330f21ca459a9ae3af2109460da8d34f45dc
yunhuiGwak <kwakyh17@naver.com>
2023-02-03 17:08:13 +0900

    추가: Todo 조회 페이지로의 이동(PageRequestDTO Link) 조회페이지에서 다시 목록으로 이동 시 기존페이지를 볼 수 있도록 하기 위해, 조회페이지로 이동 시 page,size를 같이 전달하도록 페이지이동정보를 메소드로 작성(getLink())

--- src/main/java/com/example/todo/dto/PageRequestDTO.java
+++ src/main/java/com/example/todo/dto/PageRequestDTO.java
@@ -26,9 +26,21 @@ public class PageRequestDTO {
     @Positive
     private int size = 10;  // 한 페이지당 보여주는 데이터의 수
 
+    private String link;    // 페이지 이동에 필요한 링크
+
     // limit에서 사용하는 건너뛰기(skip)의 수를 getSkip()을 만들어서 사용
     public int getSkip() {
         return (page - 1) * 10; // 현재페이지가 3페이지라면 20개의 데이터를 건너뛰고 30번째의 데이터부터 보여준다.
     }
 
+    // Todo 조회페이지에서 다시 목록으로 이동 할 때 기존 페이지를 볼 수 있도록 하기 위한 페이지 이동 정보(필요한 링크 생성 시 사용)
+    public String getLink() {   // GET방식으로 페이지 이동에 필요한 링크 생성
+        if (link == null) {
+            StringBuilder builder = new StringBuilder();
+            builder.append("page=" + this.page);
+            builder.append("&size=" + this.size);
+            link = builder.toString();
+        }
+        return link;
+    }
 }
